9890e2e07e521e2ebf0fa94ce5d67d0ffd3381f1
Maraya20 <marayakaneva@gmail.com>
2025-02-23 21:12:26 +0200

    add

--- TestApp.UnitTests/FibonacciTests.cs
+++ TestApp.UnitTests/FibonacciTests.cs
@@ -7,12 +7,32 @@ public class FibonacciTests
     [Test]
     public void Test_CalculateFibonacci_ZeroInput()
     {
-        // TODO: finish the test
+        //Arrange
+        int n = 0;
+        int expectedResult = 0;
+
+        //Act
+        int actualResult = Fibonacci.CalculateFibonacci(n);
+
+        //Assert
+
+        Assert.AreEqual(expectedResult, actualResult);
     }
 
     [Test]
     public void Test_CalculateFibonacci_PositiveInput()
     {
-        // TODO: finish the test
+        //Arrange
+        int n = 2;
+        int expectedResult = 1;
+
+        //Act
+        int actualResult = Fibonacci.CalculateFibonacci(n);
+
+        //Assert
+
+        Assert.AreEqual(expectedResult, actualResult);
     }
 }
+
+    
